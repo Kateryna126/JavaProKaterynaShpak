92190d40d5d9515ca91a7155bf538e3a0ea8b6a0
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-11 19:49:08 +0300

    added Car class with methods

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+        // Code to start electricity
+    }
+
+    private void startCommand() {
+        // Code to start command
+    }
+
+    private void startFuelSystem() {
+        // Code to start fuel system
+    }
+}
