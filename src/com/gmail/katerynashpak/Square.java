4ac7d534b7f8aff2da7e1798d9c96a80c3c65777
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-17 11:08:12 +0300

    added Square class

---
+++ src/com/gmail/katerynashpak/Square.java
@@ -0,0 +1,14 @@
+package com.gmail.katerynashpak;
+
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(side, 2);
+    }
+}
