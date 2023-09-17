5fac7afaaa373ed35eaf95eb715b0f15f084f73f
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-17 10:38:44 +0300

    added Dog class with parameters

---
+++ src/com/gmail/katerynashpak/Dog.java
@@ -0,0 +1,27 @@
+package com.gmail.katerynashpak;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= 500) {
+            System.out.println(getName() + " ran " + distance + " m");
+        } else {
+            System.out.println(getName() + " can`t run " + distance + " m");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= 10) {
+            System.out.println(getName() + " swam " + distance + " m");
+        } else {
+            System.out.println(getName() + " can't swim " + distance + " m");
+        }
+
+    }
+}
