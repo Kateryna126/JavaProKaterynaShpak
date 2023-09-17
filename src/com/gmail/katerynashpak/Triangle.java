3d0c7097b04f5b7f602b4de8d06405762e199ecd
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-17 11:06:07 +0300

    added Triangle class

---
+++ src/com/gmail/katerynashpak/Triangle.java
@@ -0,0 +1,17 @@
+package com.gmail.katerynashpak;
+
+public class Triangle implements Shape {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
