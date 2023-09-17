c8e5477852755aa5ffe1d9191070b2bf35e28c8b
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-17 10:32:05 +0300

    added Animal class with parameters

--- src/com/gmail/katerynashpak/Animal.java
+++ src/com/gmail/katerynashpak/Animal.java
@@ -2,4 +2,17 @@ package com.gmail.katerynashpak;
 
 abstract class Animal {
 
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
 }
