04b49b18d2fff9d68a1bb2aa4e9dd10aeb4e3913
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-17 11:33:53 +0300

    added Main class

---
+++ src/com/gmail/katerynashpak/Main.java
@@ -0,0 +1,19 @@
+package com.gmail.katerynashpak;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Shape>shapes = Arrays.asList(
+                new Circle(5),
+                new Triangle(3,4,5),
+                new Square(4)
+        );
+        ShapeCalculator shapeCalculator = new ShapeCalculatorImpl();
+        double totalArea = shapeCalculator.calculateTotalArea(shapes);
+        System.out.println(format("The total area of all shapes: %.2f", totalArea));
+    }
+}
