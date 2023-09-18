57de75594c3a49ce866e5f8f90f7df117e1124ea
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-09-18 21:06:57 +0300

    added checkSumSign() method

--- src/com/gmail/katerynashpak/HomeWorkApp.java
+++ src/com/gmail/katerynashpak/HomeWorkApp.java
@@ -3,6 +3,7 @@ package com.gmail.katerynashpak;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign(5,3);
     }
 
     public static void printThreeWords() {
@@ -10,6 +11,19 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
+    public static void checkSumSign(int a, int b) {
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("The sum is positive");
+        } else {
+            System.out.println("The sum is negative");
+        }
+    }
+
+
+}
 }
 
 
